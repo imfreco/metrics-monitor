c45340f1791e020b45e014557b2e1e644177628c
fredycorts7 <fred.cor.14@gmail.com>
2021-04-14 09:10:43 -0500

    [ADD]: definition of the agent and metric models

---
+++ metrics-monitor-db/lib/db.js
@@ -0,0 +1,12 @@
+'strict'
+
+const Sequelize = require('sequelize')
+let sequelize = null
+
+module.exports = function setupDatabase (config) {
+  if (!sequelize) {
+    sequelize = new Sequelize(config)
+  }
+
+  return sequelize
+}
