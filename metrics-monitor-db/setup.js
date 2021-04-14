a2e08ee0f1b33bf41d19dd95a0455186fbf002fe
fredycorts7 <fred.cor.14@gmail.com>
2021-04-14 11:19:11 -0500

    [ADD]: prompt to confirm the destruction of the database

--- metrics-monitor-db/setup.js
+++ metrics-monitor-db/setup.js
@@ -1,9 +1,23 @@
 'use strict'
 
 const debug = require('debug')('metricsmonitor:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 const db = require('./')
 
+const prompt = inquirer.createPromptModule()
+
 async function setup () {
+  const answer = await prompt([{
+    type: 'confirm',
+    name: 'setup',
+    message: 'This will destroy your database, are you sure?'
+  }])
+
+  if (!answer.setup) {
+    return console.log('Nothing happened!')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'metricsmonitor',
     username: process.env.DB_USER || 'admonitor',
@@ -22,7 +36,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red("[fatal error]")} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
